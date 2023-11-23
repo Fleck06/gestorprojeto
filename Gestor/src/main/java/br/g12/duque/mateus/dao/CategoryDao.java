e3a6d940f430bd88a7a56b5f19ef5870abcb2a39
mateusfleck <mateus.fleck@duque.g12.br>
2023-11-22 21:11:27 -0300

    Sim

--- Gestor/src/main/java/br/g12/duque/mateus/dao/CategoryDao.java
+++ Gestor/src/main/java/br/g12/duque/mateus/dao/CategoryDao.java
@@ -5,17 +5,21 @@
 package br.g12.duque.mateus.dao;
 
 import br.g12.duque.mateus.gestor.Conexao;
-import br.g12.duque.mateus.gestor.InterBanco;
 import br.g12.duque.mateus.models.Category;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.swing.SpringLayout;
+import br.g12.duque.mateus.gestor.repositories.IRespositoryCategory;
 
 /**
  *
  * @author 08137
  */
-public class CategoryDao  implements InterBanco{
+public class CategoryDao  implements IRespositoryCategory{
 
     private Category category;
     
@@ -46,21 +50,91 @@ public class CategoryDao  implements InterBanco{
 
     @Override
     public boolean update() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        //1. Definir o comando sql que será executaedo
+        String sql = "UPDATE categories SET "
+                +"SET name =?, description = ?"
+                +"WHERE id = ?";
+        //2. Definir uma variavel que dirá se a operação foi bem sucedida
+        boolean success = false;
+        //3. Conectar a aplicação no banco de dados
+        Connection conn = Conexao.getConnection();
+        //4. Fazer o tratamento de possiveis erros da aplicação
+        try{
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setString(1, category.getName());
+            pst.setString(2, category.getDescription());
+            pst.setInt(3, category.getId());
+            pst.executeUpdate();
+            success = true;
+        }catch(SQLException ex){
+            System.out.println("Erro na operação"+ ex.getMessage());
+            success = false;
+        }
+        
+        //5. Avisa o aplicativo se deu certo ou errado
+        return success;
     }
 
     @Override
     public boolean delete() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        //1. Definir o comando sql que será executaedo
+        String sql = "DELETE FROM categories WHERE id=?";
+        //2. Definir uma variavel que dará se a operação foi bem sucedida
+        boolean success = false;
+        //3. Conectar a aplicação com o BD
+        Connection conn = Conexao.getConnection();
+        //4. Fazer o tratamento de possiveis erros
+        try{
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setInt(1, category.getId());
+            pst.executeUpdate();
+            success = true;
+        }catch(SQLException ex){
+            System.out.println("Erro ao excluir o registro."
+            +"Mengagem do servidor:"+ ex.getMessage());
+            success = false;
+        }
+        //5. Avisa o aplicativo se deu certo ou errado
+        return success;
+        
     }
 
     @Override
-    public boolean findAll() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public ArrayList findAll() {
+        //1. Criar uma lista que armazenará todas as categorias
+        ArrayList<Category> list = new ArrayList<Category>();
+        
+        //2. Definir uma variavel que dirá se a operação foi bem sucedida
+        
+        boolean success = false;
+        //3. Definir qual o comando sql será executado
+        String sql = "SELECT Id, name, description FROM categories";
+        
+        //4. Estabelecer conexão com o banco de dados.
+        Connection conn = Conexao.getConnection();
+        
+        //5. Tratar os possiveis erros ou exceções que podem ocorrer
+        try{
+            //6. Enviar a query para o banco 
+            Statement stm = conn.createStatement();
+            //7. O banco retorna o resultado da consulta
+            ResultSet rs = stm.executeQuery(sql);
+            //8. Salvar o resultado da query dentro da lista
+            while(rs.next()){
+                list.add(new Category(
+                        rs.getInt("Id"),
+                        rs.getString("name"),
+                        rs.getString("description")));
+            }
+        }catch(SQLException ex){
+            System.out.println("Erro: "+ ex.getMessage());
+        }
+        
+        return list;
     }
 
     @Override
-    public boolean findById(int id) {
+    public Category findById(int id) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
--- Gestor/src/main/java/br/g12/duque/mateus/gestor/repositories/IRespositoryCategory.java
+++ Gestor/src/main/java/br/g12/duque/mateus/gestor/repositories/IRespositoryCategory.java
@@ -2,18 +2,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package br.g12.duque.mateus.gestor;
+package br.g12.duque.mateus.gestor.repositories;
+
+import br.g12.duque.mateus.models.Category;
+import java.util.ArrayList;
 
 /**
  *
  * @author 08137
  */
-public interface InterBanco {
+public interface IRespositoryCategory {
     public boolean insert(); // Métodos abstratos // Clean code
     public boolean update();
     public boolean delete();
-    public boolean findAll();
-    public boolean findById(int id); // parametros ==> variaveis locais
+    public ArrayList findAll();
+    public Category findById(int id); // parametros ==> variaveis locais
     /*
     public boolean save(){
         Customer cust = new Customer();
